d2047d02abb8f7f4db28946dbdc5ded71bc4099c
ETA_HUYNQ <huynq@ethan-tech.com>
2022-09-12 16:49:53 +0700

    test call save

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use("/static", express.static(path.join(__dirname, "public")));
 // app.use(express.static('public'))
 
 /**
@@ -77,4 +77,43 @@ app.get("/", async (req, res) => {
 app.use("/", router);
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
+});
+
+app.get("/test", async function (req, res) {
+  var level = parseInt(req.query.level) || 0;
+  console.log("server level:", level);
+  const sql = require("mssql/msnodesqlv8");
+
+  var dataTB = [];
+
+  var config = {
+    user: "sa",
+    password: "123456",
+    database: "SagawaCoreSystem",
+    driver: "msnodesqlv8",
+    server: "localhost",
+    options: {
+      trustServerCertificate: true, // change to true for local dev / self-signed certs
+    },
+  };
+  await sql.connect(config, function (err, conn) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("done connecting");
+    var request = new sql.Request();
+    request.query(
+      "select top(1) * from RRK_MST_TRH_TDB",
+      function (err, recordSet) {
+        if (err) {
+          console.log(err, "error connecting tb");
+        } else {
+          console.log(recordSet, "done connecting db");
+          // dataTB = recordSet
+          res.send(recordSet);
+        }
+      }
+    );
+  });
 });
